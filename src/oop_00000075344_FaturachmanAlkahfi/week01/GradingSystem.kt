25709e27bbda75ac2715ca772e14ffb251e2c94e
Vinzz2303 <faturachman.alkahfi@student.umn.ac.id>
2026-02-06 23:31:35 +0700

    week01: add calculateStatus function

--- src/oop_00000075344_FaturachmanAlkahfi/week01/GradingSystem.kt
+++ src/oop_00000075344_FaturachmanAlkahfi/week01/GradingSystem.kt
@@ -1,5 +1,7 @@
 package oop_00000075344_FaturachmanAlkahfi.week01
 
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name = "alvinJK"
     val score = 80
@@ -13,4 +15,5 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
