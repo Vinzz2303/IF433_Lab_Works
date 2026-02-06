d41bac4c69ee8fa92ea2486da2c9fcad07fe48c0
Vinzz2303 <faturachman.alkahfi@student.umn.ac.id>
2026-02-06 23:03:54 +0700

    week01: initial setup and procedural code

--- src/oop_00000075344_FaturachmanAlkahfi/week01/GradingSystem.kt
+++ src/oop_00000075344_FaturachmanAlkahfi/week01/GradingSystem.kt
@@ -1,2 +1,9 @@
 package oop_00000075344_FaturachmanAlkahfi.week01
 
+fun main() {
+    var name: String = "alvinJK"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
