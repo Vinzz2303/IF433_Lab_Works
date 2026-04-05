4bf7456d40ac7cc56271899a27cf6045a3047cd1
Vinzz2303 <faturachman.alkahfi@student.umn.ac.id>
2026-04-06 02:00:50 +0700

    week06: create compound interfaces

---
+++ src/oop_00000075344_FaturachmanAlkahfi/week06/Capabilities.kt
@@ -0,0 +1,13 @@
+package oop_00000075344_FaturachmanAlkahfi.week06
+
+interface Chargeable {
+    fun charge() {
+        println("Perangkat sedang diisi daya.")
+    }
+}
+
+interface Connectable {
+    fun connectToPhone() {
+        println("Perangkat terhubung ke smartphone.")
+    }
+}
