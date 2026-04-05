84fd0e07ec0b782e0daa2f0b167d030282f423f5
Vinzz2303 <faturachman.alkahfi@student.umn.ac.id>
2026-04-06 02:00:51 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000075344_FaturachmanAlkahfi/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000075344_FaturachmanAlkahfi.week06
+
+interface Camera {
+    fun capture() {
+        println("Mengambil foto dari kamera.")
+    }
+}
+
+interface Phone {
+    fun capture() {
+        println("Mengambil foto dari aplikasi telepon.")
+    }
+}
